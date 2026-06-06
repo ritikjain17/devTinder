bedc1e0c94cd0e36785f5dfdaf26ec32ec412152
ritik jain <ritik.jain@saarathi.ai>
2026-06-07 02:35:22 +0530

    error handlers

--- src/app.js
+++ src/app.js
@@ -4,35 +4,58 @@ const {adminAuth , userAuth} = require("./middleware/auth")
 
 const app = express();
 
-app.use("/admin" , adminAuth)
 
-app.get("/admin/getAllData",  (req, res) => {
-    res.send("All data is provided succesfully!");
+app.use("/" , (err, req, res, next) => {
+    console.log("Reached!")
+    res.send("Something Went Wrong")
 })
 
-app.delete("/admin/deleteData" , (req, res) => {
-    res.send("data deleted successfully");
+app.get('/user' , (req, res) => {
+
+    // try{
+        throw new Error("tdyhuijmkolpkjihugyft");
+        res.send("respone data send");
+    // }catch{
+    //     res.status(501).send("Internal Server Error!");
+    // }
+})
+
+app.use("/" , (err, req, res, next) => {
+    console.log("Reached!")
+    res.send("Something Went Wrong")
 })
 
 
-app.get("/user/getAllData" , userAuth,  (req, res) => {
-    res.send("All data is provided succesfully!");
-})
+
+// app.use("/admin" , adminAuth)
+
+// app.get("/admin/getAllData",  (req, res) => {
+//     res.send("All data is provided succesfully!");
+// })
+
+// app.delete("/admin/deleteData" , (req, res) => {
+//     res.send("data deleted successfully");
+// })
+
+
+// app.get("/user/getAllData" , userAuth,  (req, res) => {
+//     res.send("All data is provided succesfully!");
+// })
 
 // GET /user => middleware => response middleware;
-app.use("/" , (req, res , next) => {
-    console.log("route 1");
-    next()
-})
+// app.use("/" , (req, res , next) => {
+//     console.log("route 1");
+//     next()
+// })
 
 
-app.get('/user' , (req, res, next) => {
-    console.log("This will act as middleware 1");
-    next();
-}, 
-(req, res, next) => {
-    res.send("This will be response handler !")
-})
+// app.get('/user' , (req, res, next) => {
+//     console.log("This will act as middleware 1");
+//     next();
+// }, 
+// (req, res, next) => {
+//     res.send("This will be response handler !")
+// })
 
 app.listen(7777, () => {
     console.log("server is listening from the port : 7777");
