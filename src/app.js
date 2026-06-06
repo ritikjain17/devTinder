ca99babc60f84f7e6b4444aba23ee685e1d05d16
ritik jain <ritik.jain@saarathi.ai>
2026-06-06 21:06:41 +0530

    regex and route handles understanding

--- src/app.js
+++ src/app.js
@@ -2,13 +2,36 @@ const express = require("express");
 
 const app = express();
 
-// app.use('/' , (req, res)=> {
-//     res.send("Namaste Guys  --")
-// })
+
+
+// this is the regex where B is option and there so many regex type thing in express 4 version
+app.get("/ab?c" , (req, res) => {
+    res.send('E is optional')
+})
+
+
+// this will only handle the get call of /hello
+app.get('/hello' , (req, res) => {
+    res.send("Get call from the 7777")
+})
+
+app.post('/hello' , (req, res)=> {
+    res.send({Name:"Ritik" , Mobile:"67875678876"})
+})
+
+app.delete('hello' , (req, res) => {
+    res.send("User Deleted Successfully");
+})
+
+
+// this will match all the http methods api calls to /hello
+app.use('/hello/123' , (req, res)=> {
+    res.send("Hello from the server hello 1234")
+})
+
 app.use('/hello' , (req, res)=> {
     res.send("Hello from the server --")
 })
-
 
 app.use('/test' , (req, res)=> {
     res.send("Response from the server --")
@@ -18,5 +41,8 @@ app.listen(7777, () => {
     console.log("server is listening from the port : 7777");
 })
 
+// app.use('/' , (req, res)=> {
+//     res.send("Namaste Guys  --")
+// })
 
 console.log("Making a Dev Tinder of backend here!")
