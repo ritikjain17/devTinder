ec55d9ca1b7c088c628153d89b45b4aa986d5643
ritik jain <ritik.jain@saarathi.ai>
2026-06-06 23:36:44 +0530

    middleware concept

--- src/app.js
+++ src/app.js
@@ -3,84 +3,24 @@ const express = require("express");
 const app = express();
 
 
-
-// there can be a multiple route handlers -
-
-// we can make an array of mutiple route handles 
-// app.use('/Dataget', [rh1, rh2 , rh3, rh4] , rh5, rh6)
-app.get("/getData" , (req, res, next) => {
-    console.log("route handles 1 !")
-    // res.send("route 1")
+// GET /user => middleware => response middleware;
+app.use("/" , (req, res , next) => {
+    console.log("route 1");
     next()
-}, 
-[
-(req, res , next) => {
-    console.log("route handler 2 !");
-    // res.send("route 2")
-    next()
+})
+
+
+app.get('/user' , (req, res, next) => {
+    console.log("This will act as middleware 1");
+    next();
 }, 
 (req, res, next) => {
-    console.log("route handler 3 !");
-    // res.send("route 2")
-    next()
-}],
-(req, res, next) => {
-    console.log("route handler 4 !");
-    res.send("route 4")
+    res.send("This will be response handler !")
 })
-
-
-
-
-// this is the regex where B is option and there so many regex type thing in express 4 version
-app.get("/ab?c" , (req, res) => {
-    res.send('E is optional')
-})
-
-app.get('/user/:userId/:userName/:userMobile' , (request,res) => {
-    console.log(request.params);
-    res.send('getting the data of the user')
-})
-
-app.get("/user" , (req,res) => {
-    console.log(req.query)
-    res.send("getting query items")
-})
-
-
-// this will only handle the get call of /hello
-app.get('/hello' , (req, res) => {
-    res.send("Get call from the 7777")
-})
-
-app.post('/hello' , (req, res)=> {
-    res.send({Name:"Ritik" , Mobile:"67875678876"})
-})
-
-app.delete('hello' , (req, res) => {
-    res.send("User Deleted Successfully");
-})
-
-
-// this will match all the http methods api calls to /hello
-app.use('/hello/123' , (req, res)=> {
-    res.send("Hello from the server hello 1234")
-})
-
-app.use('/hello' , (req, res)=> {
-    res.send("Hello from the server --")
-})
-
-app.use('/test' , (req, res)=> {
-    res.send("Response from the server --")
-}) 
 
 app.listen(7777, () => {
     console.log("server is listening from the port : 7777");
 })
 
-// app.use('/' , (req, res)=> {
-//     res.send("Namaste Guys  --")
-// })
 
 console.log("Making a Dev Tinder of backend here!")
