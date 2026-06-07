5047e8c298be9e5aebc6dac664701d8b1e7dad5d
ritik jain <ritik.jain@saarathi.ai>
2026-06-07 22:44:21 +0530

    get, post , patch , delete API

--- src/app.js
+++ src/app.js
@@ -7,31 +7,56 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/getUser", async (req, res) => {
+  // const users = new User(req.body);
+  try {
+    // console.log("users" , users)
+    const users = await User.findOne({ email: "vishal@gmail.com" });
+    res.send(users);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("something Went Wrong");
+  }
+});
 
-app.get("/getUser" , async (req, res) => {
-    // const users = new User(req.body);
-    try{
-        // console.log("users" , users)
-        const users = await User.findOne({email:"vishal@gmail.com"})
-        res.send(users)
-    }catch(error){
-        console.log(error);
-        res.status(400).send("something Went Wrong")
-    }
-})
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    console.log(data);
+    const users = await User.findByIdAndUpdate(userId, data);
+    console.log("user ---", users);
+    res.send("Data updated successfully!");
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("something Went Wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    console.log("user id ---", userId);
+    const users = await User.findByIdAndDelete(userId);
+    res.send("user deleted Successfully!");
+  } catch (error) {
+    res.status(400).send("something went wrong!");
+  }
+});
 
 // Get All the documents from the user using find method
-app.get("/feed" , async (req, res) => {
-    // const users = new User(req.body);
-    try{
-        // console.log("users" , users)
-        const users = await User.find({})
-        res.send(users)
-    }catch(error){
-        console.log(error);
-        res.status(400).send("something Went Wrong")
-    }
-})
+app.get("/feed", async (req, res) => {
+  // const users = new User(req.body);
+  try {
+    // console.log("users" , users)
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("something Went Wrong");
+  }
+});
 
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
